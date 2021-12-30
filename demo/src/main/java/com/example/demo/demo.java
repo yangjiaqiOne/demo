587887d26a357f4b089116df6677915c5f91c995
yangjiaqi <yang80033@126.com>
2021-12-30 14:29:54 +0800

    add

--- demo/src/main/java/com/example/demo/demo.java
+++ demo/src/main/java/com/example/demo/demo.java
@@ -1,4 +1,16 @@
 package com.example.demo;
 
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/app")
 public class demo {
+
+    @GetMapping("/test")
+    public String getCode() {
+        return "hello request";
+    }
 }
